ca314555305375e06f2860efa7063903774229c0
Student <example@example.com>
2019-11-21 11:27:57 -0800

    add google api

--- src/main.js
+++ src/main.js
@@ -2,73 +2,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { DinoService, Word, GiphyService } from './dino-hangman';
-
-export function winning() {
-    console.log('inside?');
-    $('.everything').hide();
-    $('#putGiphy').attr('src', 'https://media.tenor.com/images/46c9c1ac2d73668880d4ead82d35cf57/tenor.gif');
-    $('#loser').text('YOU WIN!');
-}
+import loadGoogleMapsApi from 'load-google-maps-api';
 
 $(document).ready(function() {
-    let word, giphyService;
-    let counter = 0;
-    $('.everything').toggle();
-    $('#inputLetter').submit(function(event){
-        event.preventDefault();
-        const letter = $("#userInput").val();
-        
-        
-        const output = word.checkLetter(letter);
-        if (output) {
-            displayLetters();
-        } else {
-            counter++;
-            displayImage();
-        }
-    });
-    
-    (async () => {
-        giphyService = new GiphyService();
-        const response = await giphyService.getRandomGif();
-        giphyService.setImages(response.data);
-        //getElements(response);
-    })();
-    
-    (async () => {
-        let dinoService = new DinoService();
-        const response = await dinoService.getRandomWord();
-        word = new Word(response);
-        getElements(response);
-    })();
-
-    function getElements(response){
-        $("#word").text(response);
-        for (let i=0; i< word.getBlanks(); i++) {
-            $("#boxes").append(`<div class='item' id=${i}>-</div>`)
-        }
-        word.fillWithBlanks();
-    }
-
-    function displayLetters() {
-        console.log(word.currentState);
-        for (let i=0; i< word.currentState.length; i++) {
-            $("#" + i).text(word.currentState[i])
-        }
-    }
-
-    function displayImage() {
-        let index = Math.ceil(Math.random() * 25);
-        console.log(giphyService.images);
-        $('#putGiphy').attr('src', giphyService.images[index].images.downsized_large.url);
-
-        if(counter == 6){
-            $('.everything').toggle();
-            $('#loser').text('YOU LOSE!');
-            $('#hangman').attr('src', 'img/6.png');
-        } else {
-            $('#hangman').attr('src', `img/${counter}.png`);
-        }
-    }
+ 
+    loadGoogleMapsApi({'key':'AIzaSyB-Mgt8OR12z9WUmehS8HIQoCq_ItoMV_U'}).then(function (googleMaps) {
+        new googleMaps.Map(document.querySelector('.map'), {
+          center: {
+            lat: 47.608013,
+            lng: -122.335167
+          },
+          zoom: 12
+        })
+      }).catch(function (error) {
+        console.error(error)
+      })
 });
